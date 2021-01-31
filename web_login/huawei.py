df96853000cf95b5c483a7bd5746af2a42027d2d
xin <mynamecoder@163.com>
2021-02-01 00:39:31 +0800

    fix:linux华为签到user-agent默认为手机端

--- web_login/huawei.py
+++ web_login/huawei.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 import time
 import requests
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
 
 from selenium.common.exceptions import NoSuchElementException
 
@@ -8,15 +13,30 @@ account_list = [{"username": "mynamecoder@163.com", "password": "lovetoday#29"},
                 {"username": "17092163015", "password": "coder#1024"}]
 options = webdriver.ChromeOptions()
 # todo linux 需要设置 有界面或者无界面
-# options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"')
+
 
 
 
 def sign(browser):
     # try:
-       browser.find_element_by_id("homeheader-signin").click()
+    #    browser.find_element_by_id("homeheader-signin").click()
+    # 获取当前页的句柄
+    main_windows = browser.current_window_handle
+    # 获取所有打开的句柄
+    all_windows = browser.window_handles
+    # 循环获取到的句柄，如果不等于当前页的句柄则切换到此句柄，因为页面进行跳转，但是句柄仍停留在第一页，所以切换到新页句柄进行操作
+    for handle in all_windows:
+        if handle != main_windows:
+            browser.switch_to.window(handle)
+    # 打印页面信息
+    print(browser.page_source)
+    WebDriverWait(browser, 20).until(
+        EC.visibility_of_element_located((By.ID, "homeheader-signin"))).click()
     # except Exception as e:
         # sign(browser)
         # print(e)
