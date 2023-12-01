1639139da1985895690f0b187f715115c7aa0949
coderxm <coderxm@163.com>
2023-12-01 18:39:13 +0800

    爬取高校详情

--- dj/school_rank_detail.py
+++ dj/school_rank_detail.py
@@ -3,11 +3,12 @@ import time
 import json
 
 '''
-爬取高校排名
+爬取高校排名-详情
 '''
 
 # 原始请求：https://lx-appapi-v2.eiceducation.com.cn/api/v2/SchoolRank?type=2&keyword=&countryNames=%E8%8B%B1%E5%9B%BD%2C%E7%BE%8E%E5%9B%BD&minRank=&maxRank=&page=1&limit=10
 request_url='https://lx-appapi-v2.eiceducation.com.cn/api/v2/SchoolRank?page={}&limit={}'
+request_detail_url='https://m.eic.org.cn/api/school?ID={}'
 
 school_list=[]
 page=1
@@ -21,6 +22,8 @@ limit=len(result_json['data'])
 limit=len(result_json['data'])
 # 获取result_json['data']，类型为list的长度
 print('每页返回记录数：',limit)
+school_detail_dict = {}
+
 
 for i in range(total//limit):
     result = requests.get(request_url.format(i+1,limit))
@@ -30,11 +33,19 @@ for i in range(total//limit):
     print('page:',i+1)
     time.sleep(1)
     # 将school放入list中，然后输出到文件
-    school_list.extend(result_json['data'])
-    if i==2:
-        break
-
-with open('school_rank.json', 'w') as f:
-    json.dump(school_list, f,ensure_ascii=False)
-    f.close()
-    print('school_rank.json saved')
+    for school in result_json['data']:
+        if school['SchoolId'] in school_detail_dict or school['SchoolId'] is None:
+            continue
+        current_request_detail_url = request_detail_url.format(school['SchoolId'])
+        # print(current_request_detail_url,school['Id'])
+        time.sleep(2)
+        result = requests.get(current_request_detail_url)
+        # print(result.json())
+        json_obj = json.loads(result.json())
+        # print(json.dumps(json_obj['rows'],ensure_ascii=False))
+        if 'rows' in json_obj:
+            school_detail_dict[school['SchoolId']] = json.dumps(json_obj['rows'],ensure_ascii=False)    
+    # break
+# 遍历字典的值
+for value in school_detail_dict.values():
+    print(value)
